e621373c5f45e633d46f0b517f1ea63d6d89f2f1
ismailAbdelwahab <is.abdelwahab@gmail.com>
2020-06-10 22:02:27 +0200

    Adding sorting functions

--- App/Data_structs/SetT.py
+++ App/Data_structs/SetT.py
@@ -3,7 +3,7 @@ class SetT():
     def __init__(self):
         self.set = []
         self.nbPoints = 0
-######################### Manage points ##############################
+################### Add and Remove points ############################
     def addPoint(self, point):
         self.set.append( point )
         self.nbPoints += 1
@@ -16,25 +16,33 @@ class SetT():
                 return
         print("Point "+str(p)+" not found in set, cannot remove it.")
 ########################### Sort set #################################
+    # Initial possibilities to sort:
     def sortByX(self):
         self.set.sort( key = lambda point : point.x )
     def sortByY(self):
         self.set.sort( key = lambda point : point.y )
-
+    def sortByDecX(self):
+        self.set.sort( key = lambda point : point.x, reverse = True)
+    def sortByDecY(self):
+        self.set.sort( key = lambda point : point.y, reverse = True)
+    # Sorting by X THEN by Y:
     def sortIncXIncY(self):
-        self.sortByY()
-        self.sortByX()
+        self.sortByY(); self.sortByX()
+    def sortIncXDecY(self):
+        self.sortByDecY(); self.sortByX()
+    def sortDecXIncY(self):
+        self.sortByY(); self.sortByDecX()
     def sortDecXDecY(self):
-        self.set.sort( key = lambda point: point.y, reverse = True )
-        self.set.sort( key = lambda point: point.x, reverse = True )
-
-    def sortDecYIncX(self):
-        self.set.sort( key = lambda point:  point.x )
-        self.set.sort( key = lambda point:  point.y, reverse = True )
+        self.sortByDecY(); self.sortByDecX()
+    # Sorting by Y THEN by X:
+    def sortIncYIncX(self):
+        self.sortByX(); self.sortByY()
     def sortIncYDecX(self):
-        self.set.sort( key = lambda point:  point.x, reverse = True )
-        self.set.sort( key = lambda point:  point.y )
-
+        self.sortByDecX(); self.sortByY()
+    def sortDecYIncX(self):
+        self.sortByX(); self.sortByDecY()
+    def sortDecYDecX(self):
+        self.sortByDecX(); self.sortByDecY()
 ####################### Copy  ########################################
     def copy(self):
         copy = SetT()
